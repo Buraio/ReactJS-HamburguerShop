a1f89d2e2caa90af9b1910db72ba16f6f465be36
Bryan <bryanbatista79688@gmail.com>
2022-11-30 17:25:47 -0300

    feat: toastify, media query fix, filter by input change, empty cart component

---
+++ src/components/Cart/NoProduct/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StyledNoProductDiv from "./style";
+
+const NoProduct = () => {
+  return (
+    <StyledNoProductDiv>
+      <h2>Sua sacola está vazia</h2>
+      <p>Adicione itens</p>
+    </StyledNoProductDiv>
+  );
+};
+
+export default NoProduct;
